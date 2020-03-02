62e69ba66e806ab8407218b029c799a704bf08fd
Qiming Wang <mingqiwangcn@gmail.com>
2020-03-02 12:56:10 -0600

    add get and put

--- storage.h
+++ storage.h
@@ -1,16 +1,26 @@
 #ifndef STORAGE_H
 #define STORAGE_H
+#include <stdio.h>
 
-#define KEY_SIZE 116
+#define MAX_KEY_SIZE 116
 #define MAX_PATH_SIZE 200
 
 typedef struct DB {
     char path[MAX_PATH_SIZE];
-      
+    FILE* f_meta;
+    FILE* f_data; 
 } DB;
 
+typedef struct DataItem {
+    char key[MAX_KEY_SIZE];
+    char* value;
+} DataItem
 
 DB* db_open(char* path);
+
+void db_batch_get(DB* db, char** keys, int key_size);
+
+void db_batch_put(DB* db, DataItem* db_items, int item_size);
 
 void db_close(DB* db);
 
