a2b7af902ea42cfc78fba954eec5a0ad02d0aaa2
Qiming Wang <mingqiwangcn@gmail.com>
2020-03-09 00:15:59 -0500

    in process

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-all:
-	g++ -g -o testcase testcase.c storage.c page_manager.c python_interface.c
-py:
-	g++ -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` python_interface.c storage.c page_manager.c -o db_storage`python3-config --extension-suffix`
+db:
+	g++ -g -o testcase testcase.c storage.c page_manager.c db_interface.c
+py_db:
+	g++ -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` db_interface.c storage.c page_manager.c -o db_storage`python3-config --extension-suffix`
+py_cluster:
+	g++ -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` cluster_interface.c storage.c page_manager.c -o db_cluster`python3-config --extension-suffix`
+
 clean:
 	rm  testcase 
---
+++ testcase_interop_cluster.py
@@ -0,0 +1,65 @@
+import argparse;
+import db_storage as db;
+import random
+
+def test_put(args):
+    opts = {}
+    db.open(args.db_path, opts)
+
+    N = 10000;
+    data = []
+    data_map = {}
+    key_lst = []
+    for i in range(N):
+        key = str(i)
+        key_lst.append(key)
+        M = random.randint(10, 100);
+        value = [random.uniform(1.0, 9.9) for _ in range(M)]
+        pair = (key, value)
+        data.append(pair);
+        data_map[key] = value
+
+    db.put(data);
+ 
+    stat_data = db.stats();
+    
+    db.close()
+
+    # open again to read
+    db.open(args.db_path, opts)
+
+    stat_data = db.stats();
+
+    qry_keys = [];
+    for i in range(100):
+        pos = random.randint(0, N-1); 
+        qry_keys.append(key_lst[pos]);
+
+    query_result = db.get(qry_keys)
+    
+    for i in range(100):
+        key = qry_keys[i]
+        value1 = data_map[key]
+        value2 = query_result[i]
+
+        if value1 != value2:
+            print("error");
+            raise
+
+    db.close()    
+    print("OK")
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--db_path', type=str)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args();
+    test_put(args)
+
+if __name__ == '__main__':
+    main();
+
